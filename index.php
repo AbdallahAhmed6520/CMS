7d695298f97f1691480995a63f65e5a54e4e145e
abdosedroho <abdosedroho@gmail.com>
2022-01-25 20:03:10 +0200

    adding images in index file

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
+                $post_date  = $row['post_date'];
                 $post_image = $row['post_image'];
                 $post_content = $row['post_content'];
             ?>
@@ -33,7 +33,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images\<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
