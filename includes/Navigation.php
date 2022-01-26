86c22d8888002bab72febfc84f794ef0d35cf05a
abdosedroho <abdosedroho@gmail.com>
2022-01-26 17:05:14 +0200

    Creating reuseable code in the admin

--- includes/Navigation.php
+++ includes/Navigation.php
@@ -2,8 +2,7 @@
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -23,15 +22,8 @@
                     echo "<li><a href='#'>{$cat_title}</a></li>";
                 }
                 ?>
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Services</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Contact</a>-->
-<!--                </li>-->
+                <li><a href="admin">Admin</a></li>
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
